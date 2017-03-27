74623f9c390a3cbfcd5869f78944c361227ab43e
marioconcilio <marioconcilio@gmail.com>
2017-03-27 16:53:14 -0300

    Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# ruby encoding: utf-8
+#
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'json'
+
+json = JSON.parse(File.read("db/jupiterweb.json"))["TODOS"]
+print json.to_json
