4f072054d098717884ec76db2ecb05ba2c0b6ebd
marioconcilio <marioconcilio@gmail.com>
2017-03-29 13:13:51 -0300

    Refactoring

--- app/serializers/api/v1/classroom_serializer.rb
+++ app/serializers/api/v1/classroom_serializer.rb
@@ -1,9 +1,9 @@
 class Api::V1::ClassroomSerializer < ActiveModel::Serializer
   attributes :code, 
-  			 :date_begin, 
-  			 :date_end, 
-  			 :kind, 
-  			 :schedules
+             :date_begin, 
+             :date_end, 
+             :kind, 
+             :schedules
 
   belongs_to :subject
   has_many :schedules
