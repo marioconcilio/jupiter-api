c3da3004c1a8d4de5b8c1ce4233aa2ab548c7b9f
marioconcilio <marioconcilio@gmail.com>
2017-03-30 13:16:00 -0300

    Formatting time on Schedule model

--- app/serializers/api/v1/schedule_serializer.rb
+++ app/serializers/api/v1/schedule_serializer.rb
@@ -1,8 +1,16 @@
 class Api::V1::ScheduleSerializer < ActiveModel::Serializer
-  attributes :week_day, 
-             :time_begin, 
-             :time_end, 
+  attributes :week_day,
+             :time_begin,
+             :time_end,
              :teachers
 
   belongs_to :classroom
+
+  def time_begin
+    object.time_begin.strftime("%H:%M")
+  end
+
+  def time_end
+    object.time_end.strftime("%H:%M")
+  end
 end
