e45daf955392d7718a8c6f67172caf3690c68211
marioconcilio <marioconcilio@gmail.com>
2017-04-11 11:30:05 -0300

    Finished parsing campus

--- bin/seeds.rb
+++ bin/seeds.rb
@@ -33,9 +33,7 @@ def parse_schedule(table)
     week_day = tds[0]
     time_begin = tds[1]
     time_end = tds[2]
-
-    teachers = []
-    teachers.push(tds[3])
+    teachers = [].push(tds[3])
 
     # procura outros professores nas linhas abaixo
     # ex: seg 14:00 16:00 ProfA
@@ -46,11 +44,14 @@ def parse_schedule(table)
       tds = rows[j].search('td').map { |td| td.text.strip }
 
       # se linha contem dia da semana
-      # pula e comeca outra agenda
+      # eh outra agenda
+      # sai para o laço externo e começa outra agenda
       break if tds[0].length > 1
 
       # adiciona o professor ao array
       teachers.push(tds[3])
+
+      # guarda o indice j para usar no laço externo
       last_index = j
     end
 
@@ -78,21 +79,40 @@ def parse_campus(table)
   i = 0
   while i < rows.count
     tds = rows[i].search('td').map { |td| td.text.strip }
-    kind = tds[0]
+    data = tds.from(1).map { |td| td.to_i }
 
+    # name = ''
+    kind = tds[0]
+    # vacancies = data[0]
+    # inscribed = data[1]
+    # pending = data[2]
+    # enrolled = data[3]
+
+    # flag para checar se existe campus nas linhas abaixo
+    has_more_lines = false
+
+    # procura por campus nas linhas abaixo
+    # ex: Obrigatoria             66 60  1 59
+    #         EACH - SI - Diurno  66 42  1 41
     last_index = i
     (i+1..rows.length-1).each do |j|
+      has_more_lines = true
       tds = rows[j].search('td').map { |td| td.text.strip }
+
+      # se a linha contem Obrigatoria ou Optativa
+      # eh um novo campus
+      # sai para o laço externo e comeca outro campus
       break if tds[0].include? 'Obrigatória' && 'Optativa'
 
       data = tds.from(2).map { |td| td.to_i }
 
+      name = tds[1]
       vacancies = data[0]
       inscribed = data[1]
       pending = data[2]
       enrolled = data[3]
-      name = tds[1]
 
+      # guarda o indice j para usar no laço externo
       last_index = j
 
       puts " - name: #{name}",
@@ -104,7 +124,25 @@ def parse_campus(table)
     " ------------------------------ "
     end
 
-    # retoma da ultima linha visitada em busca de professores
+    # se nao teve nenhuma linha abaixo com descricao do campus
+    # adiciona novo campus sem nome, mas com os dados
+    unless has_more_lines
+      name = ''
+      vacancies = data[0]
+      inscribed = data[1]
+      pending = data[2]
+      enrolled = data[3]
+
+      puts " - name: #{name}",
+    " - kind: #{kind}",
+    " - vacancies: #{vacancies}",
+    " - inscribed: #{inscribed}",
+    " - pending: #{pending}",
+    " - enrolled: #{enrolled}",
+    " ------------------------------ "
+    end
+
+    # retoma da ultima linha visitada em busca de campus
     # evitando duas visitas a mesma linha
     i = last_index + 1
   end
